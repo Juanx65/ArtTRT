3ac0428bdb2d05f7d65174ae6368277a1a328858
juanx65 <juan.aguileraca@sansano.usm.cl>
2023-11-15 20:12:23 -0300

    resnet18 update

--- main_all.sh
+++ main_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # remeber to do a chmod +x main_all.sh befor runing with ./main_all.sh
-BATCH_SIZE=1
+BATCH_SIZE=128
 
-NETWORK="yolo"
+NETWORK="resnet18"
 
 MONITOR_OUTPUT_VANILLA="outputs/gpu_usage/gpu_usage_vanilla.csv"
 MONITOR_OUTPUT_FP32="outputs/gpu_usage/gpu_usage_fp32.csv"
@@ -13,7 +13,7 @@ MONITOR_OUTPUT_INT8="outputs/gpu_usage/gpu_usage_int8.csv"
 #VANILLA
 nvidia-smi dmon -i 0 -s cmu -d 1 -o TD -f "$MONITOR_OUTPUT_VANILLA" &
 NVIDIA_SMI_PID=$!
-python main.py -v --batch_size=$BATCH_SIZE --dataset='datasets/dataset_val/val' --network="$NETWORK" --less --weights weights/yolov8n-cls.pt --engine weights/yolov8n-cls.engine --model_version "Vanilla"
+python main.py -v --batch_size=$BATCH_SIZE --dataset='datasets/dataset_val/val' --network="$NETWORK" --less --engine weights/best_fp32.engine --model_version "Vanilla"
 kill $NVIDIA_SMI_PID
 python post_processing/gpu_metrics_plotter.py --csv "$MONITOR_OUTPUT_VANILLA"
 
