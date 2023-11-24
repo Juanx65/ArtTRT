1884693bc6804925ef28e08b1857b78dc2ad8f16
juanx65 <juan.aguileraca@sansano.usm.cl>
2023-11-24 04:19:35 -0300

    intento de replicar red de juanjo

---
+++ utils/models/juanjo.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class CustomQuantizedNet(nn.Module):
+    def __init__(self, nx, M, nu, L, leaky):
+        super(CustomQuantizedNet, self).__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(nx, M))
+        self.layers.append(nn.LeakyReLU(negative_slope=leaky))
+        
+        for _ in range(L-1):
+            self.layers.append(nn.Linear(M, M))
+            self.layers.append(nn.LeakyReLU(negative_slope=leaky))
+        
+        self.layers.append(nn.Linear(M, nu))
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
