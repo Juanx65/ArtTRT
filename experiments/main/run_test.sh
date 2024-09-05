d7932f680dc88aa077346643e0e1363492765617
juanx65 <juan.aguileraca@sansano.usm.cl>
2024-09-05 14:32:41 -0400

    run test for all batch size

--- experiments/main/run_test.sh
+++ experiments/main/run_test.sh
@@ -3,7 +3,29 @@
 # remeber to do a chmod +x run_test.sh befor runing with ./run_test.sh
 
 # minimal main.sh test for one test, for all the tables use gen_tables.sh
-echo "# mobilenet bs 1"
 echo " "
-./experiments/main/main.sh 1 mobilenet nonbuild datasets/subdataset_val/val
+echo "# resnet152 bs 1"
+echo " "
+./main.sh 1 resnet152 build datasets/subdataset_val/val
+
+echo " "
+echo "# resnet152 bs 32"
+echo " "
+./main.sh 32 resnet152 build datasets/subdataset_val/val
+
+echo " "
+echo "# resnet152 bs 64"
+echo " "
+./main.sh 64 resnet152 nonbuild datasets/subdataset_val/val
+
+echo " "
+echo "# resnet152 bs 128"
+echo " "
+./main.sh 128 resnet152 nonbuild datasets/subdataset_val/val
+
+echo " "
+echo "# resnet152 bs 256"
+echo " "
+./main.sh 256 resnet152 nonbuild datasets/subdataset_val/val
+
 echo " "
