1b5ac3f54c8302342d371b084b6da755993985bf
juanx65 <juan.aguileraca@sansano.usm.cl>
2024-09-05 14:48:42 -0400

    path fix

--- experiments/main/gen_tables.sh
+++ experiments/main/gen_tables.sh
@@ -6,151 +6,151 @@
 
 echo "# mobilenet bs 1"
 echo " " 
-./main.sh 1 mobilenet build datasets/dataset_val/val
+./experiments/main/main.sh 1 mobilenet build datasets/dataset_val/val
 
 echo " "
 echo "# mobilenet bs 32"
 echo " " 
-./main.sh 32 mobilenet build datasets/dataset_val/val
+./experiments/main/main.sh 32 mobilenet build datasets/dataset_val/val
 
 echo " "
 echo "# mobilenet bs 64"
 echo " " 
-./main.sh 64 mobilenet nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 64 mobilenet nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# mobilenet bs 128"
 echo " " 
-./main.sh 128 mobilenet nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 128 mobilenet nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# mobilenet bs 256"
 echo " " 
-./main.sh 256 mobilenet nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 256 mobilenet nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet18 bs 1"
 echo " "
-./main.sh 1 resnet18 build datasets/dataset_val/val
+./experiments/main/main.sh 1 resnet18 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet18 bs 32"
 echo " "
-./main.sh 32 resnet18 build datasets/dataset_val/val
+./experiments/main/main.sh 32 resnet18 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet18 bs 64"
 echo " "
-./main.sh 64 resnet18 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 64 resnet18 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet18 bs 128"
 echo " "
-./main.sh 128 resnet18 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 128 resnet18 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet18 bs 256"
 echo " "
-./main.sh 256 resnet18 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 256 resnet18 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet34 bs 1"
 echo " "
-./main.sh 1 resnet34 build datasets/dataset_val/val
+./experiments/main/main.sh 1 resnet34 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet34 bs 32"
 echo " "
-./main.sh 32 resnet34 build datasets/dataset_val/val
+./experiments/main/main.sh 32 resnet34 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet34 bs 64"
 echo " "
-./main.sh 64 resnet34 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 64 resnet34 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet34 bs 128"
 echo " "
-./main.sh 128 resnet34 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 128 resnet34 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet34 bs 256"
 echo " "
-./main.sh 256 resnet34 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 256 resnet34 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet50 bs 1"
 echo " "
-./main.sh 1 resnet50 build datasets/dataset_val/val
+./experiments/main/main.sh 1 resnet50 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet50 bs 32"
 echo " "
-./main.sh 32 resnet50 build datasets/dataset_val/val
+./experiments/main/main.sh 32 resnet50 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet50 bs 64"
 echo " "
-./main.sh 64 resnet50 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 64 resnet50 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet50 bs 128"
 echo " "
-./main.sh 128 resnet50 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 128 resnet50 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet50 bs 256"
 echo " "
-./main.sh 256 resnet50 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 256 resnet50 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet101 bs 1"
 echo " "
-./main.sh 1 resnet101 build datasets/dataset_val/val
+./experiments/main/main.sh 1 resnet101 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet101 bs 32"
 echo " "
-./main.sh 32 resnet101 build datasets/dataset_val/val
+./experiments/main/main.sh 32 resnet101 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet101 bs 64"
 echo " "
-./main.sh 64 resnet101 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 64 resnet101 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet101 bs 128"
 echo " "
-./main.sh 128 resnet101 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 128 resnet101 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet101 bs 256"
 echo " "
-./main.sh 256 resnet101 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 256 resnet101 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet152 bs 1"
 echo " "
-./main.sh 1 resnet152 build datasets/dataset_val/val
+./experiments/main/main.sh 1 resnet152 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet152 bs 32"
 echo " "
-./main.sh 32 resnet152 build datasets/dataset_val/val
+./experiments/main/main.sh 32 resnet152 build datasets/dataset_val/val
 
 echo " "
 echo "# resnet152 bs 64"
 echo " "
-./main.sh 64 resnet152 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 64 resnet152 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet152 bs 128"
 echo " "
-./main.sh 128 resnet152 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 128 resnet152 nonbuild datasets/dataset_val/val
 
 echo " "
 echo "# resnet152 bs 256"
 echo " "
-./main.sh 256 resnet152 nonbuild datasets/dataset_val/val
+./experiments/main/main.sh 256 resnet152 nonbuild datasets/dataset_val/val
 
 echo " "
--- experiments/main/run_test.sh
+++ experiments/main/run_test.sh
@@ -6,26 +6,26 @@
 echo " "
 echo "# resnet152 bs 1"
 echo " "
-./main.sh 1 resnet152 build datasets/subdataset_val/val
+./experiments/main/main.sh 1 resnet152 build datasets/subdataset_val/val
 
 echo " "
 echo "# resnet152 bs 32"
 echo " "
-./main.sh 32 resnet152 build datasets/subdataset_val/val
+./experiments/main/main.sh 32 resnet152 build datasets/subdataset_val/val
 
 echo " "
 echo "# resnet152 bs 64"
 echo " "
-./main.sh 64 resnet152 nonbuild datasets/subdataset_val/val
+./experiments/main/main.sh 64 resnet152 nonbuild datasets/subdataset_val/val
 
 echo " "
 echo "# resnet152 bs 128"
 echo " "
-./main.sh 128 resnet152 nonbuild datasets/subdataset_val/val
+./experiments/main/main.sh 128 resnet152 nonbuild datasets/subdataset_val/val
 
 echo " "
 echo "# resnet152 bs 256"
 echo " "
-./main.sh 256 resnet152 nonbuild datasets/subdataset_val/val
+./experiments/main/main.sh 256 resnet152 nonbuild datasets/subdataset_val/val
 
 echo " "
